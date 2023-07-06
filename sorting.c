0eacb0e086cab74695f0b9c9a4614c901ed95b4d
sachin kittur <sachikumar1310@gmail.com>
2023-07-06 22:22:33 +0530

    i changed inside if condition

--- sorting.c
+++ sorting.c
@@ -15,9 +15,9 @@ void sort_array(int arr[],int size)
         {
             if(arr[i]>arr[j])
             {
-                 arr[i]=arr[i]+arr[j];
-                 arr[j]=arr[i]-arr[j];
-                 arr[i]=arr[i]-arr[j];
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[i]=temp;
             }
             j++;
         }
