8c630d8cac893856f7d873deec3da07612fa0a62
sachin kittur <sachikumar1310@gmail.com>
2023-07-06 11:27:59 +0530

    added fibbonacci file

---
+++ fibbonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  int n, first = 0, second = 1, result, i;
+  printf("Please give an input upto you want to print series :");
+  scanf("%d", &n);
+  printf("Fibonacci Series is: \n");
+  for (i = 0; i < n; i++)
+  {
+    if (i <= 1)
+      result = i;
+    else
+    {
+      result = first + second;
+      first = second;
+      second = result;
+    }
+    printf("%d \n", result);
+  } 
+  return 0;
+} 
