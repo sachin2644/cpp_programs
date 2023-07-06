1b7e52e3900b79bd70752c0a19e1b22b42721d37
sachin kittur <sachikumar1310@gmail.com>
2023-07-06 10:47:37 +0530

    added swap file

---
+++ swap_fun.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void swap(int *,int *);
+int main()
+{
+     int a=10,b=20;
+     swap(&a,&b);
+     printf("After swapping\na=%d\nb=%d\n",a,b);
+}
+void swap(int *a,int *b)
+{
+     int temp=*a;
+     *a=*b;
+     *b=temp;
+}
