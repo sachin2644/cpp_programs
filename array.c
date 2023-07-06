303a5514f2722162f056052f18924713126e575f
sachin kittur <sachikumar1310@gmail.com>
2023-07-06 11:36:51 +0530

    added array file

---
+++ array.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void reverseArray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+
+    while (start < end) {
+        // Swap elements at start and end indices
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        // Move the start and end indices towards the center
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    // Reverse the array
+    reverseArray(arr, size);
+
+    printf("\nReversed array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
