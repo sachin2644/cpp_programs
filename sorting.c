963f201657bac138240fd1135335526aa395418b
sachin kittur <sachikumar1310@gmail.com>
2023-07-06 10:53:10 +0530

    added sorting file

---
+++ sorting.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Comparison function for sorting integers in ascending order
+int compare(const void *a, const void *b) {
+    int num1 = *(const int *)a;
+    int num2 = *(const int *)b;
+
+    if (num1 < num2)
+        return -1;
+    else if (num1 > num2)
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+    int numbers[] = {5, 2, 8, 1, 9};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    // Sort the array
+    qsort(numbers, size, sizeof(int), compare);
+
+    // Print the sorted array
+    printf("Sorted array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
