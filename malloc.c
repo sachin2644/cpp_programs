66f698559ebd9b43dad7b8295380ca929af9e736
sachin kittur <sachikumar1310@gmail.com>
2023-07-07 07:07:52 +0530

    added malloc file

---
+++ malloc.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *ptr;
+    int size;
+    printf("Enter size :");
+    scanf("%d",&size);
+    ptr=(int *)malloc(size*sizeof(int));
+    if(ptr==NULL)
+    {
+        printf("Memory is unable to allocate\n");
+    }
+    else
+    {
+        printf("Memory successfully allocate using malloc\n");
+    }
+    printf("Enter array elements \n");
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&ptr[i]);
+    }
+    printf("resverse array elements \n");
+    for(int i=size-1;i>=0;i--)
+    {
+        printf("%d ",ptr[i]);
+    }
+    free(ptr);
+}
