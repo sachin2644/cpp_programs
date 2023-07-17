4ccce6d77ea6aa21ba089b1547afcbbd81d0622f
sachin kittur <sachikumar1310@gmail.com>
2023-07-17 07:22:39 +0530

    added swap file

---
+++ swap.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+struct swap
+{
+    int num1;
+    int num2;
+};
+int main()
+{
+    struct swap s;
+    printf("Enter two number to swap :");
+    scanf("%d%d",&s.num1,&s.num2);
+    printf("Before swapping\nnum1=%d\nnum2=%d\n",s.num1,s.num2);
+    s.num1=s.num1^s.num2;
+    s.num2=s.num1^s.num2;
+    s.num1=s.num1^s.num2;
+    printf("After swapping\nnum1=%d\nnum2=%d\n",s.num1,s.num2);
+    printf("size = %zu bytes\n",sizeof(s));
+}
